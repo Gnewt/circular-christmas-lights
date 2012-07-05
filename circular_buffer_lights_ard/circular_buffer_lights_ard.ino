87ea5b77eac9273dc9443f66f089c2c692da0d78
Gnewt <gnewt@gnewt.at>
2012-07-04 23:10:36 -0700

    Committing the wrong code is a bad habit... here's the right code.

--- circular_buffer_lights_ard/circular_buffer_lights_ard.ino
+++ circular_buffer_lights_ard/circular_buffer_lights_ard.ino
@@ -14,7 +14,7 @@ typedef struct
 } LightInfo;
 
 GEColorEffects lights(outPin, lightCount);
-int index;
+int arrayIndex;
 LightInfo lightArray[lightCount];
 int i;
 bool update = true;
@@ -24,9 +24,8 @@ void setup()
   Serial.begin(9600);
   pinMode(13, OUTPUT);
   lights.enumerate();
-  index = 0;
+  arrayIndex = 0;
   populate_circular_buffer();
-  //populate_linked_list();
   let_there_be_light();
 }
 
@@ -40,40 +39,24 @@ void loop()
   }
 }
 
-/*void populate_linked_list()
-{
-  int i;
-  rgb *curr;
-  curr = head = (rgb*)malloc(sizeof(rgb));
-  
-  for (i = 0; i < lightCount-1; i++) {
-      curr->r = 15;
-      curr->g = curr->b = 0;
-      curr->next = (rgb*)malloc(sizeof(rgb));
-      curr = curr->next;
-  }
-  curr->r = 15;
-  curr->g = curr->b = 0;
-  curr->next = head;    
-  curr = head;
-}*/
-
 void populate_circular_buffer()
 {
-  memset(&lights, 0xFF, lightCount*sizeof(LightInfo));
+  for (i = 0; i < lightCount; i++) {
+    add_color(0, 0, 15);
+  }  
 }
   
   
 void add_color(uint8_t r, uint8_t g, uint8_t b)
 {
-  lightArray[index].r = r;
-  lightArray[index].g = g;
-  lightArray[index].b = b;
-  if (index >= lightCount-1) {
-    index = 0;
+  lightArray[arrayIndex].r = r;
+  lightArray[arrayIndex].g = g;
+  lightArray[arrayIndex].b = b;
+  if (arrayIndex >= lightCount-1) {
+      arrayIndex = 0;
   }
   else {
-    index++;
+      arrayIndex++;
   }
 }
 
@@ -83,13 +66,13 @@ void let_there_be_light()
   color_t color;
   
   for (i = 0; i < lightCount; i++) {
-    color = lights.color(lightArray[index].r, lightArray[index].g, lightArray[index].b);
+    color = lights.color(lightArray[arrayIndex].r, lightArray[arrayIndex].g, lightArray[arrayIndex].b);
     lights.set_color(i, DEFAULT_INTENSITY, color);
-    if (index >= lightCount - 1) {
-      index = 0;
+    if (arrayIndex >= lightCount - 1) {
+      arrayIndex = 0;
     }
     else {
-      index++;
+      arrayIndex++;
     }
   }
 }
@@ -112,7 +95,6 @@ void handle_serial() {
   r = (combo << 4) >> 12;
   g = (combo << 8) >> 12;
   b = (combo << 12) >> 12;
-  //r = 0; g = 15; b = 0;
   add_color(r, g, b);
 }
 
